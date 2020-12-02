6be71377b185fc00c8185b2e2250432f07f710ba
bomj321 <jmob612@gmail.com>
2020-12-02 01:48:49 -0400

    ADD: New files

--- routes/task.js
+++ routes/task.js
@@ -5,13 +5,10 @@ let TaskController = require('../controllers/task');
 let router = express.Router();
 let md_auth = require('../middlewares/authenticated');
 
-//Test routes
 router.post('/task',md_auth.authenticated, TaskController.save);
 router.get('/user-tasks/:user', TaskController.getTasksByUser);
 router.get('/task/:id', TaskController.getTask);
 router.put('/task/:id', md_auth.authenticated, TaskController.update);
 router.delete('/task/:id', md_auth.authenticated, TaskController.delete)
-//router.get('/search/:search', TaskController.search);
-
 
 module.exports = router;
